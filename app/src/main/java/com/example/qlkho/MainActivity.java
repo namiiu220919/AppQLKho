9a09c152cb972b40b0e0c409dd46238026cd3f44
Nguyễn Phương Nam <121838234+namiiu220919@users.noreply.github.com>
2023-11-24 10:55:34 +0700

    V1.0

---
+++ app/src/main/java/com/example/qlkho/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.qlkho;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
